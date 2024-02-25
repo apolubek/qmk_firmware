75e7993ebb34596c8fdd5857ca8f1851eccbae86
Adam Połubek <adam.polubek@tooploox.com>
2024-02-25 11:41:05 +0100

    Further improvements

--- keyboards/sofle/keymaps/default/config.h
+++ keyboards/sofle/keymaps/default/config.h
@@ -5,4 +5,17 @@ see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
 
-#define MASTER_LEFT
+#define SPLIT_USB_DETECT
+
+#define SPLIT_USB_TIMEOUT 1500
+#define SPLIT_USB_TIMEOUT_POLL 10
+
+#define SPLIT_WATCHDOG_ENABLE
+#define SPLIT_WATCHDOG_TIMEOUT 2000
+
+#define DEBOUNCE 10
+
+#ifdef ENCODER_RESOLUTION
+    #undef ENCODER_RESOLUTION
+#endif
+#define ENCODER_RESOLUTION 4
--- keyboards/sofle/keymaps/default/rules.mk
+++ keyboards/sofle/keymaps/default/rules.mk
@@ -5,5 +5,10 @@ CONSOLE_ENABLE = no
 EXTRAKEY_ENABLE = yes
 COMBO_ENABLE = yes
 
+SPLIT_KEYBOARD = yes
+KEY_OVERRIDE_ENABLE = yes
+
+DEBOUNCE_TYPE = sym_defer_pk
+
 # for debugging using xprintf()
 # CONSOLE_ENABLE = yes
